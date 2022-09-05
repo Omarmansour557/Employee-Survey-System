8cfe04729c9d784b565e295ea5818a88e91bbb74
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2022-09-05 16:10:53 +0200

    added login required to the emplyoee view and added login/logout and signup links to the navbar

--- employee/web_views.py
+++ employee/web_views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Employee
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
-class EmployeeDetailView(DetailView):
+class EmployeeDetailView(LoginRequiredMixin, DetailView):
     model = Employee
     template_name = 'employee_detail.html'
