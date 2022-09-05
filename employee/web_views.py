71d984253a5a40f71f06718f89b6a91948de7022
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2022-09-05 16:35:36 +0200

    changed the url name of the web employee profile from profile to employee_profile and added an updateView for the employee

--- employee/web_views.py
+++ employee/web_views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView
 from .models import Employee
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
 class EmployeeDetailView(LoginRequiredMixin, DetailView):
     model = Employee
     template_name = 'employee_detail.html'
+
+class EmployeeUpdateView(LoginRequiredMixin, UpdateView, UserPassesTestMixin):
+    model = Employee
+    template_name = 'employee_update.html'
+    fields = ['name', 'department', 'job_title']
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
