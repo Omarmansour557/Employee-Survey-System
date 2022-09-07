c0fa49db3287ad610eb74c524e562f75d3fc26bb
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2022-09-07 12:03:53 +0200

    PAGTBI-15: created a list view for the employee's surveys

---
+++ survey_system/web_urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+from . import web_views
+
+urlpatterns = [
+    
+]
---
+++ survey_system/web_views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import DetailView
+from employee.models import Employee
+from django.contrib.auth.decorators import login_required
+
+from survey_system.models import EmployeeSurvey
+
+
+@login_required()
+def employee_survey_list_view(request):
+    template_name = 'survey_list.html'
+    user = request.user
+
+    if user.is_staff:
+            return redirect('admin:index')
+    elif user.is_anonymous:
+        return redirect('login')
+
+    context={
+        'survey_list': user.employee.survey_set.all()
+    }
+
+    return render(request, template_name, context)
