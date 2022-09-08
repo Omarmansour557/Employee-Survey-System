a82bf95969a76c87a8e1b698968d73e938478eab
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2022-09-08 14:23:58 +0200

    PAGTBI-20: added pagination to the employee list view

--- employee/web_views.py
+++ employee/web_views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 class EmployeeListView(LoginRequiredMixin, ListView):
     model = Employee
     template_name = 'employee_list.html'
+    paginate_by = 1
 
 class EmployeeDetailView(LoginRequiredMixin, DetailView):
     model = Employee
